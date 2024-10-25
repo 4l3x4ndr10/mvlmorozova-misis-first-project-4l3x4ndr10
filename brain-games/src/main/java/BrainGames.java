ca0e385e554a71e5e6bf357b931cb3dbab1ad7eb
4l3x4ndr10 <entakibora66@gmail.com>
2024-10-25 15:51:58 +0300

    small changes

--- brain-games/src/main/java/BrainGames.java
+++ brain-games/src/main/java/BrainGames.java
@@ -5,18 +5,24 @@ public class BrainGames {
         String name = Cli.greet();  // Приветствие и запрос имени пользователя
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose a game: 1 - LCM, 2 - Progression");
-        int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1:
-                Engine.runGame(new LcmGame(), name);
-                break;
-            case 2:
-                Engine.runGame(new ProgressionGame(), name);
-                break;
-            default:
-                System.out.println("Invalid choice");
+        while (true) {
+            System.out.println("Choose a game: 1 - LCM, 2 - Progression, 0 - Exit");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Engine.runGame(new LcmGame(), name);
+                    break;
+                case 2:
+                    Engine.runGame(new ProgressionGame(), name);
+                    break;
+                case 0:
+                    System.out.println("Goodbye, " + name + "!");
+                    return;  // Завершение программы
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
         }
     }
 }
